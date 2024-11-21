0065763a549d7f5dc3eb63d8e51b7258f4d6183d
Florian Gicquiaud <florian.gicquiaud@talan.com>
2024-11-21 18:04:25 +0100

    create components directions and preferences

---
+++ frontend-angular/src/app/components/directions/directions.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DirectionsComponent } from './directions.component';
+
+describe('DirectionsComponent', () => {
+  let component: DirectionsComponent;
+  let fixture: ComponentFixture<DirectionsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DirectionsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DirectionsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend-angular/src/app/components/directions/directions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directions',
+  imports: [],
+  templateUrl: './directions.component.html',
+  styleUrl: './directions.component.css'
+})
+export class DirectionsComponent {
+
+}
