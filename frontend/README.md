b48354a10cd590aa3bd0697ca3c418be7c4f6e18
antoniov <antonio.villarreal@talan.com>
2024-11-21 08:01:40 +0100

    Ready

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,6 @@
 To run the app run:
-npm run dev --port 7000
+npm run dev 
+
+#port 5173
 
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
       host: '127.0.0.1',
 	  proxy: {
 		'/api': {
-		  target: 'http://localhost:8000',
+		  target: 'http://10.244.1.149:8000',
 		  changeOrigin: true,
 		  rewrite: (path) => path.replace(/^\/api/, '')
 		}
